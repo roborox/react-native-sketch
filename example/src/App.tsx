ba1448add0d231d380395fea0ac160ff60f13fd5
Mikhaylov Anton <anion155@gmail.com>
2020-07-10 14:50:59 +0500

    feat: update exampe

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,102 @@
-import * as React from "react"
-import { StyleSheet, View, Text } from "react-native"
-import { SketchView, SketchToolType } from "react-native-sketch"
-import { TemporaryDirectoryPath } from "react-native-fs"
+import React, { useRef, useState, useMemo } from "react"
+import { StyleSheet, View, Text, Button } from "react-native"
+import {
+  SketchView,
+  SketchToolType,
+  SketchViewRef,
+  PenSketchTool,
+  EraseSketchTool,
+} from "react-native-sketch"
+import { ExternalDirectoryPath } from "react-native-fs"
 
 export default function App() {
+  const sketch = useRef<SketchViewRef>(null)
+
+  const [penTool, setPenTool] = useState<PenSketchTool>({
+    color: "blue",
+    thickness: 30,
+  })
+  const [eraseTool, setEraseTool] = useState<EraseSketchTool>({
+    thickness: 20,
+  })
+  const tools = useMemo(() => {
+    return {
+      [SketchToolType.pen]: penTool,
+      [SketchToolType.erase]: eraseTool,
+    }
+  }, [eraseTool, penTool])
+
+  const [tool, setTool] = useState<SketchToolType>(SketchToolType.pen)
+
   return (
     <View style={styles.container}>
       <Text>Bah</Text>
-      <SketchView
-        selectedTool={SketchToolType.pen}
-        localSourceImagePath={TemporaryDirectoryPath + "/draw"}
-      />
+      <View style={{ flexDirection: "row" }}>
+        <Button title="Save" onPress={() => sketch.current?.saveSketch({ width: 512, height: 512 })} />
+        <Button title="Clear" onPress={() => sketch.current?.clearSketch()} />
+        <Button title="Pen" onPress={() => setTool(SketchToolType.pen)} />
+        <Button title="Erase" onPress={() => setTool(SketchToolType.erase)} />
+      </View>
+      {tool === SketchToolType.pen ? (
+        <View style={{ flexDirection: "row" }}>
+          <Button
+            title="Red"
+            onPress={() => setPenTool((t) => ({ ...t, color: "red" }))}
+          />
+          <Button
+            title="Green"
+            onPress={() => setPenTool((t) => ({ ...t, color: "green" }))}
+          />
+          <Button
+            title="Blue"
+            onPress={() => setPenTool((t) => ({ ...t, color: "blue" }))}
+          />
+        </View>
+      ) : null}
+      {tool === SketchToolType.pen ? (
+        <View style={{ flexDirection: "row" }}>
+          <Button
+            title="30"
+            onPress={() => setPenTool((t) => ({ ...t, thickness: 30 }))}
+          />
+          <Button
+            title="20"
+            onPress={() => setPenTool((t) => ({ ...t, thickness: 20 }))}
+          />
+          <Button
+            title="10"
+            onPress={() => setPenTool((t) => ({ ...t, thickness: 10 }))}
+          />
+        </View>
+      ) : null}
+      {tool === SketchToolType.erase ? (
+        <View style={{ flexDirection: "row" }}>
+          <Button
+            title="30"
+            onPress={() => setEraseTool((t) => ({ ...t, thickness: 30 }))}
+          />
+          <Button
+            title="20"
+            onPress={() => setEraseTool((t) => ({ ...t, thickness: 20 }))}
+          />
+          <Button
+            title="10"
+            onPress={() => setEraseTool((t) => ({ ...t, thickness: 10 }))}
+          />
+        </View>
+      ) : null}
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <SketchView
+          ref={sketch}
+          toolType={tool}
+          tool={tools[tool]}
+          filePath={ExternalDirectoryPath + "/draw"}
+          style={{ flex: 1, opacity: 0.5 }}
+          onLoad={(e) => console.log("loaded:", e)}
+          onSaved={(e) => console.log("saved:", e)}
+          onChange={() => console.log("changed")}
+        />
+      </View>
     </View>
   )
 }
